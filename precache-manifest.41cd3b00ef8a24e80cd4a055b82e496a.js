3ce782e24a8501fbd246538d4ad817290f157db6
Graham Cottridge <codinggc@gmail.com>
2019-03-11 22:40:49 +0000

    Updates

---
+++ precache-manifest.41cd3b00ef8a24e80cd4a055b82e496a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "156cf5b10d56406c8ced",
+    "url": "/react-contact-manager/static/js/runtime~main.156cf5b1.js"
+  },
+  {
+    "revision": "f45280b3e1e70c4f3400",
+    "url": "/react-contact-manager/static/js/main.f45280b3.chunk.js"
+  },
+  {
+    "revision": "f18e2bfa370c3cf57abc",
+    "url": "/react-contact-manager/static/js/2.f18e2bfa.chunk.js"
+  },
+  {
+    "revision": "f45280b3e1e70c4f3400",
+    "url": "/react-contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "f18e2bfa370c3cf57abc",
+    "url": "/react-contact-manager/static/css/2.fe6aee6b.chunk.css"
+  },
+  {
+    "revision": "d1f6070bcdaeca307b4f911596ccc1f4",
+    "url": "/react-contact-manager/index.html"
+  }
+];
